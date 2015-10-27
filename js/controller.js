58ce388a49f9190eeeb3912350e7d5c2e63dbd94
Erin Meyers <erinmeyers88@gmail.com>
2015-10-27 12:29:07 -0600

    Finished project.

---
+++ js/controller.js
@@ -0,0 +1,12 @@
+angular.module("userProfiles")
+	.controller("mainController", function ($scope, mainService) {
+		
+			$scope.getUsers = function () {
+				$scope.users = mainService.getUsers();
+			};
+			
+			$scope.getUsers();
+			
+	
+	});
+	
