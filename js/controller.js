e355848b618a7bf3c4071088686cd5661165c431
Erin Meyers <erinmeyers88@gmail.com>
2015-10-28 11:59:03 -0600

    Updated data source to API.

--- js/controller.js
+++ js/controller.js
@@ -2,7 +2,10 @@ angular.module("userProfiles")
 	.controller("mainController", function ($scope, mainService) {
 		
 			$scope.getUsers = function () {
-				$scope.users = mainService.getUsers();
+				mainService.getUsers().then(function(response){
+					$scope.users = response.data.data;
+				})
+				
 			};
 			
 			$scope.getUsers();
