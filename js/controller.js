a53054361d33a3d9dff121b0fa80e5bdb5562749
Erin Meyers <erinmeyers88@gmail.com>
2015-10-29 12:08:33 -0600

    Added $q functionality.

--- js/controller.js
+++ js/controller.js
@@ -1,31 +1,31 @@
 angular.module("userProfiles")
 	.controller("mainController", function ($scope, mainService) {
-		
-	$scope.currentPage = 1;
-	$scope.maxPages = 4;	
-		
-		
-			$scope.getUsers = function (pageNum) {
-				$scope.users = mainService.getUsers(pageNum).then(function(response){
-					$scope.users = response.data;
-					$scope.maxPages = response.total_pages;
-				});
-				
-			}
-	
-	$scope.next = function () {
-		$scope.currentPage++;
-		$scope.getUsers($scope.currentPage);
-		
-	}
-	
-	
-	$scope.previous = function () {
-		$scope.currentPage--;
-		$scope.getUsers($scope.currentPage);
-	}
-	
-	$scope.getUsers();
-	
+
+		$scope.currentPage = 1;
+		$scope.maxPages = 4;
+
+
+		$scope.getUsers = function (pageNum) {
+			mainService.getUsers(pageNum).then(function (dataFromService) {
+				$scope.users = dataFromService;
+				$scope.maxPages = dataFromService.total_pages;
+			});
+
+		};
+
+		$scope.next = function () {
+			$scope.currentPage++;
+			$scope.getUsers($scope.currentPage);
+
+		}
+
+
+		$scope.previous = function () {
+			$scope.currentPage--;
+			$scope.getUsers($scope.currentPage);
+		}
+
+		$scope.getUsers();
+
 	});
 	
--- js/service.js
+++ js/service.js
@@ -1,12 +1,21 @@
 angular.module("userProfiles")
-	.service("mainService", function ($http) {
+	.service("mainService", function ($http, $q) {
                 
 	this.getUsers = function (pageNum) {
-            return $http({
+            var deferred = $q.defer();
+            
+            $http({
                 method: 'GET',
                 url: 'http://reqres.in/api/users?page=' + pageNum
             }).then(function (response) {
-                    return response.data;
-            });  
-        };
-	});
+                    var parsedResponse = response.data.data
+                    
+                    for (var i =0; i < parsedResponse.length; i++) {
+                            parsedResponse[i].first_name = "Ralf";
+                    }
+                    
+                    deferred.resolve(parsedResponse);
+            })  
+            return deferred.promise;
+        }
+});
